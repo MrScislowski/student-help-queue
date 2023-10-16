44fb4b6d5ed0f733c22dbcdd4058802088d457ae
Daniel Scislowski <mr.scislowski@gmail.com>
2023-10-16 09:49:01 -0500

    add types to frontend

--- frontend/app/types.ts
+++ frontend/app/types.ts
@@ -1,3 +1,14 @@
+export type ResolutionStatus = "cancel" | "resolve";
+
+export interface ActiveEntry {
+  _id: string;
+  request: {
+    user: User;
+    timestamp: string;
+  };
+  queueName: string;
+}
+
 export interface User {
   email: string;
   givenName: string;
