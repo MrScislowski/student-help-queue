b19c9e0fcd65219e0f0787fbf834e90215575b12
Daniel Scislowski <mr.scislowski@gmail.com>
2023-11-17 06:24:57 -0600

    add templates for service functions

--- backend/src/services/activeQueueService.ts
+++ backend/src/services/activeQueueService.ts
@@ -1,4 +1,40 @@
+import { Account, ActiveQueue, ResolutionStatus, User } from "../types";
+import { AccountModel } from "../models/account";
+
 // get active entries
-// hide/show/rename a queue
+const getEntriesByEndpoint(endpoint: string) : ActiveQueue[] => {
+
+}
+
+// add an entry
+const addActiveEntry(user: User, endpoint: string, queueId: string) : void => {
+
+}
+
+
 // resolve an entry
+const resolveEntry(user: User, endpoint: string, queueId: string, resolutionStatus: ResolutionStatus): void => {
+
+}
+
 // add a queue
+const addQueue(owner: Owner, queueName: string) : void => {
+
+}
+
+// hide/show/rename/remove a queue
+const hideQueue(owner: Owner, queueId: string) : void => {
+
+}
+
+const showQueue(owner: Owner, queueId: string) : void => {
+
+}
+
+const renameQueue(owner: Owner, queueId: string, newName: string) : void => {
+
+}
+
+const deleteQueue(owner: Owner, queueId: string) : void => {
+  
+}
