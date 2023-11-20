2135370edcc54c8ceeea0f983697f73507234430
Daniel Scislowski <mr.scislowski@gmail.com>
2023-11-20 11:06:52 -0500

    refactor middleware, implement getQueues

--- backend/src/controllers/studentRequests.ts
+++ backend/src/controllers/studentRequests.ts
@@ -1,1 +1,49 @@
-// contains requests that students may make, so that the middleware is the same for all
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Router, Request, Response, NextFunction } from "express";
+import { ActiveQueue } from "../types";
+import { parseSession } from "../utils";
+import jwt from "jsonwebtoken";
+import config from "../config";
+import activeQueueService from "../services/activeQueueService";
+
+const router = Router();
+
+const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void | Response => {
+  if (!req.headers.authorization) {
+    return res.status(400).send("Token required in authorization header");
+  }
+
+  const token = req.headers.authorization.substring(7);
+  const sessionInfo = parseSession(jwt.verify(token, config.SECRET));
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  res.locals.session = sessionInfo;
+  next();
+};
+
+router.use(authenticateToken);
+
+router.get("/:classId", async (req, res) => {
+  const classId = req.params.classId;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  try {
+    const queues: ActiveQueue[] = await activeQueueService.getQueuesForStudent(
+      classId
+    );
+
+    res.send(queues);
+  } catch (error: unknown) {
+    let message = "";
+    if (error instanceof Error) {
+      message += error.message;
+    }
+    return res.status(500).send({ error: message });
+  }
+});
+
+export default router;
