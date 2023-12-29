4d312d5361a959d8d38923ab8dbcc369f4a8cd09
Daniel Scislowski <mr.scislowski@gmail.com>
2023-12-29 15:30:13 -0600

    extract cookie name to config file

--- teacher-frontend/app/[endpoint]/App.tsx
+++ teacher-frontend/app/[endpoint]/App.tsx
@@ -8,6 +8,7 @@ import QueueSet from "./components/QueueSet";
 import { Session } from "./types";
 import { QueryClient, QueryClientProvider } from "react-query";
 import AddQueueForm from "./components/AddQueueForm";
+import config from "../config";
 
 const queryClient = new QueryClient();
 
@@ -19,7 +20,7 @@ const App = ({ classId }: AppProps) => {
   const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
-    const storedUserInfo = window.localStorage.getItem("studentHelpQueueUser");
+    const storedUserInfo = window.localStorage.getItem(config.cookieName);
     if (storedUserInfo) {
       const userInfo = JSON.parse(storedUserInfo);
       setSession(userInfo);
@@ -29,7 +30,7 @@ const App = ({ classId }: AppProps) => {
 
   // TODO: could re-implement session context to provide the setter method. Then this logout could be moved into the loginbutton component
   const handleLogout = () => {
-    window.localStorage.removeItem("studentHelpQueueUser");
+    window.localStorage.removeItem(config.cookieName);
     setSession(null);
     setToken(null);
   };
