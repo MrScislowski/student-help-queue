858ea42d029b9b9b237e1ad601fc2d56124701d2
Daniel Scislowski <mr.scislowski@gmail.com>
2024-01-02 22:50:16 -0600

    remove quotes from html

--- student-frontend/app/teachers/page.tsx
+++ student-frontend/app/teachers/page.tsx
@@ -1,5 +1,5 @@
 "use client";
 
 export default function Home() {
-  return <p>Please make sure you're using the URL your teacher gave you</p>;
+  return <p>Please make sure you use the URL your teacher gave you</p>;
 }
