66c5f13947b04ba398cffc031ac8d22e80c72c08
Daniel Scislowski <mr.scislowski@gmail.com>
2024-01-05 10:01:24 -0600

    Refactor Queue by creating QueueEntry component

--- student-frontend/components/Queue.tsx
+++ student-frontend/components/Queue.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
-import { ActiveEntry, Queue, ResolutionStatus } from "../types/types";
+import { Queue } from "../types/types";
 import { useMutation, useQueryClient } from "react-query";
-import { resolveEntry, addName } from "../utils/requests";
+import { addName } from "../utils/requests";
 import { useContext, useEffect, useState } from "react";
 import TimeOffsetContext from "./TimeOffsetContext";
 import SessionContext from "./SessionContext";
 import QueueTitle from "./QueueTitle";
+import QueueEntry from "./QueueEntry";
 
 const EntriesContainer = styled.div`
   display: flex;
@@ -15,44 +16,9 @@ const EntriesContainer = styled.div`
   border-radius: 10px;
 `;
 
-const QueueItem = styled.div`
-  background-color: #a0cfd3;
-  border-radius: 5px;
-  padding: 10px;
-  margin: 10px;
-`;
-
-const ResolveButton = styled.button`
-  background-color: rgb(233, 246, 233);
-  border: none;
-  border-radius: 5px;
-`;
-
-const CancelButton = styled.button`
-  background-color: rgb(255, 241, 223);
-  border: none;
-  border-radius: 5px;
-`;
-
 const AddNameButton = styled.button`
   border-radius: 10px;
 `;
-
-const getEntryAge = (
-  currentTime: number,
-  entryTime: number,
-  timeOffset: number
-) => {
-  const age = currentTime - (entryTime + timeOffset);
-  const minutes = Math.floor(age / 1000 / 60);
-  if (minutes < 1) {
-    return "< 1m";
-  } else if (minutes > 60) {
-    const hours = Math.floor(minutes / 60);
-    return `${hours}h ${minutes - hours * 60}m`;
-  }
-  return `${minutes}m`;
-};
 
 interface QueueProps {
   teacherSlug: string;
@@ -77,36 +43,6 @@ const Queue = (props: QueueProps) => {
     },
     onSuccess: () => {
       queryClient.invalidateQueries(["entries"]);
-    },
-  });
-
-  const resolveEntryMutation = useMutation({
-    mutationFn: ({ studentEmail }: { studentEmail: string }) => {
-      return resolveEntry(
-        teacherSlug,
-        classSlug,
-        queueId,
-        studentEmail,
-        "resolve"
-      );
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["entries"] });
-    },
-  });
-
-  const cancelEntryMutation = useMutation({
-    mutationFn: ({ studentEmail }: { studentEmail: string }) => {
-      return resolveEntry(
-        teacherSlug,
-        classSlug,
-        queueId,
-        studentEmail,
-        "cancel"
-      );
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["entries"] });
     },
   });
 
@@ -135,41 +71,17 @@ const Queue = (props: QueueProps) => {
       )}
 
       <EntriesContainer>
-        {entries.map((item) => {
+        {entries.map((entry) => {
           return (
-            <QueueItem key={item.user.email}>
-              {item.user.givenName} {item.user.familyName} (
-              {getEntryAge(
-                currentTime,
-                new Date(item.timeAdded).getTime(),
-                timeOffset
-              )}
-              )
-              {item.user.email === session.user.email ? (
-                <>
-                  <ResolveButton
-                    onClick={() => {
-                      resolveEntryMutation.mutate({
-                        studentEmail: item.user.email,
-                      });
-                    }}
-                  >
-                    Resolve
-                  </ResolveButton>
-                  <CancelButton
-                    onClick={async () => {
-                      cancelEntryMutation.mutate({
-                        studentEmail: item.user.email,
-                      });
-                    }}
-                  >
-                    Cancel
-                  </CancelButton>
-                </>
-              ) : (
-                <></>
-              )}
-            </QueueItem>
+            <QueueEntry
+              key={entry.user.email}
+              teacherSlug={teacherSlug}
+              classSlug={classSlug}
+              queueId={queueId}
+              entry={entry}
+              currentTime={currentTime}
+              timeOffset={timeOffset}
+            />
           );
         })}
       </EntriesContainer>
